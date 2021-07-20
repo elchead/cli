3ca767267899a8529e5d983e1a747a233687bd75
Adrian Stobbe <adrian.stobbe@sap.com>
2021-07-20 12:04:27 +0200

    rm

--- internal/k3s/k3s_test.go
+++ internal/k3s/k3s_test.go
@@ -128,7 +128,6 @@ func TestClusterExists(t *testing.T) {
 
 func TestArgConstruction(t *testing.T) {
 	rawPorts := []string{"8000:80@loadbalancer", "8443:443@loadbalancer"}
-
 	res := constructArgs("-p", rawPorts)
 	require.Equal(t, []string{"-p", "8000:80@loadbalancer", "-p", "8443:443@loadbalancer"}, res)
 }
